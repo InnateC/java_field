eb2681e4c7400987c43d77e2bfe7d38abcce74dc
InnateC <Denis567@i.ua>
2020-11-07 11:04:41 +0200

    First commit

---
+++ src/main/java/com/innatecoder/java_field/App.java
@@ -0,0 +1,13 @@
+package com.innatecoder.java_field;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
