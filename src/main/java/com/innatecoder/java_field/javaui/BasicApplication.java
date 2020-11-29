460c8d07289ac82cb429625e1879ee3b476641eb
InnateC <Denis567@i.ua>
2020-11-29 12:49:54 +0200

    edit

--- src/main/java/com/innatecoder/java_field/javaui/BasicApplication.java
+++ src/main/java/com/innatecoder/java_field/javaui/BasicApplication.java
@@ -2,13 +2,18 @@ package com.innatecoder.java_field.javaui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
 
 public class BasicApplication extends Application {
 
     @Override
         public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("My First JavaFX App");
-        
+        Label label = new Label("Hello World, JavaFX!");
+        Scene scene = new Scene(label, 400, 200);
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
     
