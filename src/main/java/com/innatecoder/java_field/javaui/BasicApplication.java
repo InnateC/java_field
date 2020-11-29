ab094e0245b8fe4849677e31ef205f317f899016
InnateC <Denis567@i.ua>
2020-11-29 11:05:41 +0200

    Switched to more stable version

---
+++ src/main/java/com/innatecoder/java_field/javaui/BasicApplication.java
@@ -0,0 +1,20 @@
+package com.innatecoder.java_field.javaui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BasicApplication extends Application {
+
+    @Override
+        public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First JavaFX App");
+        
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    
+
+}
--- src/main/java/com/innatecoder/java_field/javaui/LabelClass.java
+++ src/main/java/com/innatecoder/java_field/javaui/LabelClass.java
@@ -1,11 +1,11 @@
 /* example from CLI
 export PATH_TO_FX=/usr/lib/jvm/java-14-openjdk/lib/
-javac -d target/classes --module-path $PATH_TO_FX --add-modules javafx.controls src/main/java/com/innatecoder/java_field/javafx/LabelClass.java
+javac -d target/classes --module-path /usr/lib/jvm/java-14-openjdk/lib/ --add-modules javafx.controls src/main/java/com/innatecoder/java_field/javafx/LabelClass.java
 java -cp target/classes --module-path $PATH_TO_FX --add-modules javafx.controls com.innatecoder.java_field.javafx.LabelClass
 */ 
 
 
-package com.innatecoder.java_field.javafx;
+package com.innatecoder.java_field.javaui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -19,10 +19,12 @@ public class LabelClass extends Application {
 
     @Override
     public void start(Stage stage) {
+                
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         Scene scene = new Scene(new StackPane(l), 640, 480);
+        
         stage.setScene(scene);
         stage.show();
     }
