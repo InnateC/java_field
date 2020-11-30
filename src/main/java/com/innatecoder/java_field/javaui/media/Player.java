75965073cf91e8b6ed6f3d02a9fcdcfab5f46424
InnateC <Denis567@i.ua>
2020-11-30 20:35:43 +0200

    minor edit

--- src/main/java/com/innatecoder/java_field/javaui/media/Player.java
+++ src/main/java/com/innatecoder/java_field/javaui/media/Player.java
@@ -1,12 +1,5 @@
 package com.innatecoder.java_field.javaui.media;
 
-
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -18,13 +11,6 @@ public class Player extends Application {
         public void start(Stage primaryStage) {
             try {
                 
-                //primaryStage.setTitle("Media Player");
-                //var mediafile = new File("src/main/java/com/innatecoder/java_field/javaui/media/Portal.mp4");
-                
-                //var media = new Media(mediafile.toURI().toURL().toString());
-                //var mediaPlayer = new MediaPlayer(media);
-                                
-                                
                 Stage stage = FXMLLoader.load(getClass().getResource("media.fxml"));
                 stage.show();
                 
