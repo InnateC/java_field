ab69be0f8d71e797937f5f0d5e318c2290c61ee0
InnateC <Denis567@i.ua>
2020-12-01 17:26:57 +0200

    edited

--- src/main/java/com/innatecoder/java_field/concurrency/Math_algo0.java
+++ src/main/java/com/innatecoder/java_field/concurrency/Math_algo0.java
@@ -1,8 +1,11 @@
 package com.innatecoder.java_field.concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
+// Static type
 class Data {
 
     public static int[] fill(int numOfElements) {
@@ -16,10 +19,33 @@ class Data {
     }
 }
 
+class DataGeneric {
+
+    public static <T> List<T> fill(int numOfElements, T elem) {
+
+        List<T> data = new ArrayList<T>();
+        
+        
+        //Random random = new Random();
+        
+        for(int i = 0; i < numOfElements; i++)
+            data.add(elem);
+
+        return data;
+    }
+}
+
+
+
+   
+
+
 
 public class Math_algo0 {
-    public static void main(String[] args) {
-        var arr = Data.fill(20);
-        System.out.println(arr[2]);
+
+     public static void main(String[] args) {
+        //var arr = Data.fill(20);
+        var arr = DataGeneric.fill(10, 5);
+        System.out.println(arr.toString());
 }
 }
